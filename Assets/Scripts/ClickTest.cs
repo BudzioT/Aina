5508c02ae927018e20ab13f9b34dd18e0e918a91
budziot <budzio.zsm1@gmail.com>
2025-10-05 11:54:19 +0200

    Added model but kinda doesn't show up in editor

--- Assets/Scripts/ClickTest.cs
+++ Assets/Scripts/ClickTest.cs
@@ -1,11 +1,13 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ClickTest : MonoBehaviour
+public class ClickTest : MonoBehaviour, IPointerClickHandler
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
     }
 
     // Update is called once per frame
@@ -13,9 +15,22 @@ public class ClickTest : MonoBehaviour
     {
         
     }
+    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Debug.LogError("Rotated via UI click");
+        transform.Rotate(0f, 0f, 30f);
+    }
+    
+    public void Clicked()
+    {
+        //Debug.LogError("Rotated via UI click");
+        transform.Rotate(0f, 0f, 30f);
+    }
 
     private void OnMouseDown()
     {
+        //Debug.LogError("Rotated via OnMouseDown");
         transform.Rotate(0f, 0f, 30f);
     }
 }
