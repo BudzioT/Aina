6cbf3f50843fa606a74c70dc9b8a0879664ff244
budziot <budzio.zsm1@gmail.com>
2025-10-06 20:31:36 +0200

    Moving character!

--- Assets/Scripts/Assistant/MoveItem.cs
+++ Assets/Scripts/Assistant/MoveItem.cs
@@ -1,16 +1,42 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MoveItem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Mouse pos to track... mouse
+    private Vector3 mousePos;
+    // Offset: characterPos (world) - mousePos (world). Basically offset to where you click character, so that it
+    // doesn't teleport, but moves according to offset
+    private Vector3 offset;
+    
+    private Camera mainCam;
+    
+    
     void Start()
     {
-        
+        mainCam = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    // Track offset where player clicked the character, didn't work in OnMouseDrag
+    private void OnMouseDown()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        offset = transform.position - mousePos;
+    }
+
+    // Move character according to offset, works fine!!!!
+    private void OnMouseDrag()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 oldPos = transform.position;
+        
+        transform.position = mousePos + offset;
+        Debug.LogError($"Offset: {offset}, Old pos: {oldPos}, New: {transform.position}, MousePos: {mousePos}");
     }
 }
